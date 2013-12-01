53cb55a5a252db8f1c684fdbd403ca715b46feaf
Hasan Ahmad <hasan.ibrahim.ahmad@gmail.com>
2013-11-30 20:12:19 -0500

    initial commit

---
+++ JavaFXHello.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+
+public class JavaFXHello extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override public void start(Stage stage) {
+		Label label = new Label("Hello World");
+		Scene scene = new Scene(label);
+		stage.setScene(scene);
+		stage.show();
+	}
+}
